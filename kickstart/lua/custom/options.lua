2960cb8c79e47a6588ed2eea50b255fab5a1de80
logandavies181 <logan.davies181@gmail.com>
2024-08-30 23:07:50 +1200

    slightly lighter grey

--- kickstart/lua/custom/options.lua
+++ kickstart/lua/custom/options.lua
@@ -1,3 +1,4 @@
+vim.g.sonokai_colors_override = {grey = {"#9fa4b0", "246"}}
 vim.g.sonokai_disable_italic_comment = 1
 vim.cmd("colorscheme sonokai")
 vim.cmd("highlight! link TSPunctBracket yellow")
