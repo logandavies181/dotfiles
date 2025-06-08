af6987d53faa064f6da653279f83c730420a35a7
logandavies181 <logan.davies181@gmail.com>
2025-06-08 17:30:36 +1200

    use path

--- kickstart/lua/graphqlzp.lua
+++ kickstart/lua/graphqlzp.lua
@@ -2,7 +2,7 @@ require("lspconfig.configs").graphqlzp = {
   default_config = {
     enabled = true,
     filetypes = {"graphql"},
-    cmd = { "/home/logie/src/graphqlzp/zig-out/bin/graphqlzp" },
+    cmd = { "graphqlzp" },
     root_dir = function (name)
       return require("lspconfig.util").root_pattern({"*.graphql", ".git"})(name)
     end
